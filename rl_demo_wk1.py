e6867da69bd1c8cb8617c42f98020eef4a630f9e
ATF3-evergreen <atf3@evergreen.edu>
2023-04-06 19:26:43 +0000

    working

--- rl_demo_wk1.py
+++ rl_demo_wk1.py
@@ -13,7 +13,7 @@ also use rand.gauss(0, 1) instead of i
 
 import random as rand
 
-N = 2
+N = 1000
 
 def average1():
     sum = 0
@@ -30,8 +30,10 @@ def average2():
 
 def average3():
     avg = 0
+    
     for i in range(1, N):
-        avg = i #fix this
+        val = i
+        avg = avg + (val-avg)/i
     print('avg3', avg)
 
 
